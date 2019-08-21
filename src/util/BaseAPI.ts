fb9c116830c247ddd6e7ca031fe9e31aaad237b9
Benjamin Hoover <benjamin.hoover@ibm.com>
2019-08-21 11:50:46 -0400

    Import utils from previous project

---
+++ src/util/BaseAPI.ts
@@ -0,0 +1,68 @@
+import * as d3 from 'd3';
+import * as R from 'ramda'
+
+/**
+ * Format the API URL based, assuming all backend calls are to the '/api' endpoint
+ * 
+ * @param host The host at which this app is deployed
+ */
+const urlFormat = (host) => {
+    if (host == "localhost") return "http://localhost:5000/api";
+    return `http://${host}/api`;
+}
+
+const baseurl = urlFormat(window.location.hostname)
+
+/**
+ * Create a GET URL based on an base URL and parameter object.
+ * 
+ * @param base Base URL on which to add get parameters
+ * @param params Object to add to URL for get request
+ */
+export function makeUrl(base: string, params?: object):string {
+    if (params){
+        let out: string = base + "?";
+
+        Object.keys(params).forEach( k => {
+            out += k;
+            out += '=';
+            out += params[k];
+            out += "&";
+        })
+        return out.replace(/&$/g, "");
+    }
+    else {
+        return base;
+    }
+};
+
+/**
+ * Certain character symbols are not allowed to be sent via get request. This function correctly filters out the "#" symbol
+ */
+const filterHash = R.compose(
+    R.replace(/\#/g, "%23"), 
+    R.prop('text'));
+
+/**
+ * Template class for making API calls to a backend
+ */
+export class BaseAPI {
+    constructor(private baseURL: string=null) {
+        if (this.baseURL == null) {
+            this.baseURL = baseurl;
+        }
+    }
+
+    exampleCall<T extends any>(paramA:string, paramB:string): Promise<T>{
+        const toSend = {
+            paramA: paramA,
+            paramB: paramB,
+        };
+
+        const url = makeUrl(this.baseURL + "/desired-route", toSend)
+        console.log("--- GET " + url);
+
+        return d3.json(url)
+    }
+}
+};
---
+++ src/util/xd3.ts
@@ -0,0 +1,54 @@
+import * as d3 from 'd3'
+import {BaseType} from 'd3'
+
+export type D3Sel = d3.Selection<any, any, any, any>
+export type d3S<T extends BaseType, U = any> = d3.Selection<T, U, any, any>
+
+d3.selection.prototype.clear = function() {  
+    this.selectAll('*').remove();
+    return this;
+}
+
+d3.selection.prototype.toggleClass = function(className) {  
+    this.classed(className, !this.classed(className));
+    return this;
+}
+
+d3.selection.prototype.show = function() {  
+    this.style('display', 'initial');
+    return this;
+}
+
+d3.selection.prototype.hide = function() {  
+    this.style('display', 'none');
+    return this;
+}
+
+d3.selection.prototype.toggle = function() {  
+    var isHidden = this.style('display') == 'none';
+    return this.style('display', isHidden ? 'inherit' : 'none');
+}
+
+d3.selection.prototype.after = function(tagName) {  
+    var elements = [];
+  
+    this.each(function() {
+      var element = document.createElement(tagName);
+      this.parentNode.insertBefore(element, this.nextSibling);
+      elements.push(element);
+    });
+  
+    return d3.selectAll(elements);
+  }
+
+d3.selection.prototype.before = function(tagName) {  
+    var elements = [];
+  
+    this.each(function() {
+      var element = document.createElement(tagName);
+      this.parentNode.insertBefore(element, this);
+      elements.push(element);
+    });
+  
+    return d3.selectAll(elements);
+}
