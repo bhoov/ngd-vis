3cccee22fed1ffd693fb3a9f9d3c1a96ca08357d
Benjamin Hoover <benjamin.hoover@ibm.com>
2019-08-22 17:01:04 -0400

    Move parameters into options

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import * as d3 from 'd3'
+import * as $ from 'jquery'
+import {RegressionLoss} from './ts/loss'
 import {SimpleGraph} from './ts/vis/SimpleGraph'
 
 function main() {
-    const graph = new SimpleGraph(d3.select('#vis1'))
+    // const graph = new SimpleGraph(d3.select('#vis1'))
+    const lossGraph = new RegressionLoss(d3.select('#vis1').node())
+    lossGraph.plot(0, 0)
+    console.log(lossGraph);
 }
 
 main()
--- src/ts/vis/SimpleGraph.ts
+++ src/ts/vis/SimpleGraph.ts
@@ -7,8 +7,22 @@ type T = number[]
 
 export class SimpleGraph extends SVGVisComponent<T> {
     cssname = "simple-cake"
+
     _data: T
-    options = {}
+    x
+    y
+    xaxis
+    yaxis
+    pad
+    legend
+    tip
+    color
+    contours
+
+    // Specify the grid for the contours
+    n: number = 50
+    m: number = 50
+
 
     constructor(d3parent: D3Sel, eventHandler?: SimpleEventHandler, options={}) {
         super(d3parent, eventHandler, options)
@@ -17,7 +31,29 @@ export class SimpleGraph extends SVGVisComponent<T> {
     }
 
     init() { 
-        this.base
+        const op = this.options
+
+        this.contours = d3.contours().size([this.n, this.m])
+        this.color = d3.scaleLog().interpolate(() => d3.interpolateYlGnBu);
+        this.x = d3.scaleLinear().range([0, op.width]).domain([-5, 5])
+        this.y = d3.scaleLinear().range([0, op.height]).domain([-5, 5])
+
+        this.svg
+            .attr("width", op.width)
+            .attr("height", op.height)
+
+        const width = op.width - op.margin.left - op.margin.right,
+            height = op.height - op.margin.top - op.margin.bottom
+
+        this.xaxis = this.base.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(this.x).ticks(3, "s"));
+        
+        this.yaxis = this.base.append("g")
+            .attr("class", "axis axis--y")
+            .attr("transform", "translate(40,0)")
+            .call(d3.axisLeft(this.y).ticks(3, "s"));
     }
 
     data():number[]
