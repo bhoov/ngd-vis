917d411b0ed0ea90ab931677426c4442a30ac972
Benjamin Hoover <benjamin.hoover@ibm.com>
2019-10-17 10:49:59 -0400

    Fix animation on golf hole problem

--- src/ts/vis/GolfHole1D.ts
+++ src/ts/vis/GolfHole1D.ts
@@ -1,13 +1,13 @@
 import * as d3 from 'd3'
-import {D3Sel} from '../util/xd3'
+import { D3Sel } from '../util/xd3'
 import * as R from 'ramda'
-import {Vector2D} from '../types'
-import {SVGOptions, SVGVisComponent} from '../util/VisComponent'
+import { Vector2D } from '../types'
+import { SVGOptions, SVGVisComponent } from '../util/VisComponent'
 import { SimpleEventHandler } from '../util/SimpleEventHandler';
-import {SVG} from '../util/SVGplus'
-import {interval, fromEvent} from 'rxjs'
-import {map, tap, take, startWith, scan, switchMap} from 'rxjs/operators'
-import {ManualUpdater} from './ManualUpdater'
+import { SVG } from '../util/SVGplus'
+import { interval, fromEvent } from 'rxjs'
+import { map, tap, take, startWith, scan, switchMap } from 'rxjs/operators'
+import { ManualUpdater } from './ManualUpdater'
 import { start } from 'repl'
 
 interface Ball {
@@ -15,7 +15,7 @@ interface Ball {
     updater: ManualUpdater
 }
 
-type T = Array<Ball>
+type T = Ball
 
 interface GraphOptions extends SVGOptions {
     xrange: [number, number]
@@ -30,27 +30,27 @@ interface GraphScales {
 }
 
 interface GraphSels {
-    xaxis?:D3Sel
-    yaxis?:D3Sel
-    xlabel?:D3Sel
-    line?:D3Sel
-    backdrop?:D3Sel
-    ball?:D3Sel
+    xaxis?: D3Sel
+    yaxis?: D3Sel
+    xlabel?: D3Sel
+    line?: D3Sel
+    backdrop?: D3Sel
+    ball?: D3Sel
 }
 
 const func = x => Math.pow(Math.tanh(x), 2)
-const dFunc = x => Math.pow(Math.cosh(x), -2)
-const ddFunc = x => -2 * Math.tanh(x) * Math.pow((1 / Math.cosh(x)), 2)
+const dFunc = x => Math.tanh(x) * Math.pow(Math.cosh(x), -2)
+const ddFunc = x => -2 * Math.tanh(x) / Math.pow(Math.cosh(x), 2)
 
 export class GolfHole1D extends SVGVisComponent<T> {
-    cssname = "multi-parabola-chart"
+    cssname = "golf-hole-chart"
 
     _data: T
 
-    options:GraphOptions = {
+    options: GraphOptions = {
         maxWidth: 450,
         maxHeight: 200,
-        margin: {top: 50, right: 75, bottom: 75, left: 50},
+        margin: { top: 50, right: 75, bottom: 75, left: 50 },
         pad: 30,
         xrange: [-7, 7],
         yrange: [0, 1.3],
@@ -63,22 +63,88 @@ export class GolfHole1D extends SVGVisComponent<T> {
 
     updater: ManualUpdater
 
-    constructor(d3parent: D3Sel, eventHandler?: SimpleEventHandler, options={}) {
+    descender // RXJS interval observable that plots the descent of a golf ball
+
+    constructor(d3parent: D3Sel, eventHandler?: SimpleEventHandler, options = {}) {
         super(d3parent, eventHandler, options)
         this.base.classed(this.cssname, true)
         this.init()
-        this.updater = new ManualUpdater(func, dFunc, ddFunc)
+        this.updater = new ManualUpdater(func, dFunc, ddFunc, 1, 1)
+        this.data({ x: 5, updater: this.updater })
+        this.initBalls()
     }
 
-    intoVis(v:Vector2D) {
-        return {x: this.scales.x(v.x), y: this.scales.y(v.y)}
+    /**
+     * Put a vector into the coordinate system used by the visualization
+     * 
+     * @param v Vector 
+     */
+    intoVis(v: Vector2D) {
+        return { x: this.scales.x(v.x), y: this.scales.y(v.y) }
     }
 
-    intoMath(v:Vector2D) {
-        return {x: this.scales.x.invert(v.x), y: this.scales.y.invert(v.y)}
+    // Take a vector in the coordinate system and return it to the math coordinates
+    intoMath(v: Vector2D) {
+        return { x: this.scales.x.invert(v.x), y: this.scales.y.invert(v.y) }
     }
 
-    init() { 
+    // Get the datapoint that would represent the next ball
+    nextBall(b: Ball): Ball {
+        const newB = R.assoc('x', b.updater.next(b.x), b)
+        return newB
+    }
+
+    // Turn a number into a vector
+    num2vec(x: number): Vector2D {
+        return { x: x, y: func(x) }
+    }
+
+    // Turn a ball into a vector
+    ball2vec(b: Ball): Vector2D {
+        return this.num2vec(b.x)
+    }
+
+    // Turn a ball into a vector in the visualization coordinate system
+    ball2vis(b: Ball) {
+        return this.intoVis(this.ball2vec(this.nextBall(b)))
+    }
+
+    // Plot a ball on the chart
+    plotBall(b: Ball, cls = 'golf-ball') {
+        const self = this;
+
+        const toClass = name => '.' + name
+        const clsSel = toClass(cls)
+
+        const ball = this.ball2vis(b)
+        self.sels.ball = self.base.selectAll(clsSel)
+            .datum(ball)
+            .join('circle')
+            .classed("cls", true)
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y)
+            .attr("r", "5px")
+    }
+
+    /**
+     * Plot the values of all x according to the function
+     * 
+     * @param x A linspace of x values over which to plot a curve
+     */
+    plotCurve(x: number[]) {
+        const self = this, scales = this.scales, sels = this.sels;
+
+        scales.paths.forEach(line => {
+            this.base.append("path")
+                .datum(x)
+                .attr("class", "line")
+                .attr("d", d => {
+                    return line(d)
+                })
+        })
+    }
+
+    init() {
         const self = this;
         const op = this.options;
         const scales = this.scales;
@@ -91,7 +157,7 @@ export class GolfHole1D extends SVGVisComponent<T> {
             .attr("width", op.maxWidth)
             .attr("height", op.maxHeight)
 
-        this.base = SVG.group(this.base, '', {x: op.margin.left, y: op.margin.top})
+        this.base = SVG.group(this.base, '', { x: op.margin.left, y: op.margin.top })
 
         scales.x = d3.scaleLinear().domain(op.xrange).range([0, op.width])
         scales.y = d3.scaleLinear().domain(op.yrange).range([op.height, 0])
@@ -100,58 +166,26 @@ export class GolfHole1D extends SVGVisComponent<T> {
             .attr("class", "axis axis--x")
             .attr("transform", SVG.translate(0, op.height))
             .call(d3.axisBottom(scales.x).ticks(3, "s"));
-        
+
         sels.xlabel = this.base.append("text")
             .text("x")
             .attr("class", "titles")
-            .attr("transform", SVG.translate(op.width/2, op.height + op.pad))
+            .attr("transform", SVG.translate(op.width / 2, op.height + op.pad))
 
         const baseLine = d3.line<number>()
-            .x((d:number, i:number) => scales.x(d))
-            .y((d:number, i:number) => scales.y(func(d)))
+            .x((d: number, i: number) => scales.x(d))
+            .y((d: number, i: number) => scales.y(func(d)))
             .curve(d3.curveLinear)
 
         scales.paths = [baseLine]
 
-        // baseLine.y((d:number, i:number) => scales.y(func(d)))
-
         const linspace = (start, end, n) => {
             const delta = (end - start) / (n - 1)
-            return d3.range(start, end+delta, delta).slice(0, n)
+            return d3.range(start, end + delta, delta).slice(0, n)
         }
 
         const xs = linspace(op.xrange[0], op.xrange[1], 1000)
-        this.plot(xs)
-        this.initBalls()
-    }
-
-    nextBalls() {
-        const self = this;
-
-        // Iterate to the next x
-        const data = this.data().map(o => {
-            return {x: o.updater.next(o.x), updater: o.updater}
-        })
-
-        return data
-    }
-
-    plotBalls() {
-        const self = this;
-
-        // Take current x, put function into visualization
-        const data = this.data().map(o => {
-            const vec = {x: o.x, y: o.updater.f(o.x)}
-            return self.intoVis(vec)
-        })
-
-        self.sels.ball = self.base.selectAll('.golf-ball')
-            .data(data)
-            .join('circle')
-            .classed("golf-ball", true)
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-            .attr("r", "5px")
+        this.plotCurve(xs)
     }
 
     initBalls() {
@@ -165,14 +199,39 @@ export class GolfHole1D extends SVGVisComponent<T> {
             .attr("height", op.height)
             .attr("width", op.width)
 
-        this.sels.backdrop.on('click', function() {
-            const click = self.intoMath(toVec(d3.mouse(this)))
-            const fVal = {x: click.x, y: func(click.x)}
-            const plotFVal = self.intoVis(fVal)
+        const subObj = {
+            next: x => console.log(x),
+            error: x => console.log("ERROR: ", x),
+            complete: () => console.log("COMPLETE"),
+        }
 
-            console.log(plotFVal);
+        const ticker = () => interval(10).pipe(
+            map(x => 1),
+            scan((acc: Ball) => {
+                const newBall = self.nextBall(acc)
+                self.data(newBall)
+                return newBall
+            }, self.data()),
+            take(1000)
+        ).subscribe(subObj)
 
-            self.data()
+        // Running ticker starts as an empty subscription object, is later replaced by the running ticker
+        let runningTicker = {
+            unsubscribe: () => console.log("Empty Ticker!")
+        }
+
+        // fromEvent(this.sels.backdrop.node(), 'click').pipe(
+        //     tap(x => console.log(x)),
+        //     startWith((e:MouseEvent) => descendSetup(e)),
+        //     switchMap(ticker)
+        // ).subscribe({next: x => x})
+
+        this.sels.backdrop.on('click', function () {
+            runningTicker.unsubscribe()
+            const click = toVec(d3.mouse(this))
+            self.data(R.assoc('x', self.intoMath(click).x, self.data()))
+            const plotFVal = self.ball2vis(self.data())
+
             self.sels.ball = self.base.selectAll('.golf-ball')
                 .data([plotFVal])
                 .join('circle')
@@ -181,47 +240,30 @@ export class GolfHole1D extends SVGVisComponent<T> {
                 .attr("cy", d => d.y)
                 .attr("r", "5px")
 
-        const subObj = {
-            next: x => console.log(x)
-        }
-
-         interval(50).pipe(
-            startWith(() => fVal),
-            tap(x => console.log(x)),
-            // @ts-ignore
-            scan(x => self.updater.next(x)),
-            tap(x => console.log(x)),
-            take(10)
-        ).subscribe(subObj)
+            runningTicker = ticker()
+            // const descendFunc = () => interval(20).pipe(
+            //     map(x => 1),
+            //     scan((acc: number) => {
+            //         const newx = self.data().updater.next(acc)
+            //         self.data(R.assoc('x', newx, self.data()))
+            //         return self.data().x
+            //     }, self.data().x),
+            //     take(100)
+            // ).subscribe(subObj)
         })
     }
 
-    data():T
-    data(val:T): this
+    data(): T
+    data(val: T): this
     data(val?) {
         if (val == null) return this._data
         this._data = val;
-        this.plotBalls();
+        this.plotBall(val);
         return this
     }
 
-
-    plot(x:number[]) {
-        const self = this, scales = this.scales, sels = this.sels;
-
-        scales.paths.forEach(line => {
-            this.base.append("path")
-                .datum(x)
-                .attr("class", "line")
-                .attr("d", d => {
-                    return line(d)
-                })
-            })
-    }
 }
 
-function toVec([x, y]:[number, number]):Vector2D {
-    return {x: x, y: y}
+function toVec([x, y]: [number, number]): Vector2D {
+    return { x: x, y: y }
 }
-
-const click2val = R.compose(toVec)
--- src/ts/vis/ManualUpdater.ts
+++ src/ts/vis/ManualUpdater.ts
@@ -1,4 +1,4 @@
-import {Vector2D} from '../types'
+import { Vector2D } from '../types'
 import * as d3 from 'd3'
 
 const defaultStep2Lr: d3.ScaleLinear<number, number> = d3.scaleLinear().domain([0, 0.8]).range([0.001, 0.25])
@@ -9,11 +9,11 @@ export class ManualUpdater {
     f: (x: number) => number
     df: (x: number) => number
     ddf: (x: number) => number
-    
+
     q: number                               // 0 -> 1, where 0 is SGD and 1 is NGD. 0.5 is sqrt NGD. [step = - eta * H ^ (-1/q) * g] (H = 0 when q=0)
     eta: number                             // aka 'learning rate'
 
-    constructor(f, df, ddf, q=0, eta=0.1) {
+    constructor(f, df, ddf, q = 0, eta = 0.1) {
         this.f = f;
         this.df = df;
         this.ddf = ddf;
@@ -22,32 +22,30 @@ export class ManualUpdater {
     }
 
     // Calculate error squared
-    loss(x: number):number{
+    loss(x: number): number {
         return 0.5 * Math.pow(this.f(x), 2)
     }
 
-    gradient(x:number):number {
+    gradient(x: number): number {
         return this.f(x) * this.df(x)
     }
 
     // Full hessian at x
-    hess(x:number):number {
+    hess(x: number): number {
         return Math.pow(this.df(x), 2) + this.f(x) * this.ddf(x)
     }
 
-    psdhess(x: number):number {
+    psdhess(x: number): number {
         return Math.pow(this.df(x), 2)
     }
 
-    step(x:number): number {
+    step(x: number): number {
         const step = y => - this.eta * y;
 
-        // Prevent divide by 0
-        if (this.q == 0) return step(this.gradient(x))
-        return step(Math.pow(this.psdhess(x), -1/this.q) * this.gradient(x))
+        return step(Math.pow(this.psdhess(x), this.q) * this.gradient(x))
     }
 
-    next(x:number): number {
+    next(x: number): number {
         return x + this.step(x)
 
     }
