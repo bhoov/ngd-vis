b9a9200d5a0cbfd2f3ab5b6315031a03dc65df87
Benjamin Hoover <benjamin.hoover@ibm.com>
2019-10-18 15:42:05 -0400

    Fix slider scale and defaults

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,117 +3,119 @@ import { ContourPlot } from './vis/ContourPlot'
 import { D3Sel } from './util/xd3'
 import { MultiParabola } from './vis/MultiParabola'
 import { GolfHole1D } from './vis/GolfHole1D'
+import * as R from 'ramda'
+
+const toFixed = R.curry((ndigits, x) => x.toFixed(ndigits))
+const toQ = toFixed(2)
+const toEta = toFixed(6)
 
 function plotQuiverGraph() {
-    const vis1 = d3.select('#vis1')
-    const sels = {
-        quiverPlot: vis1.select('#chart'),
-        qId: vis1.select('#q-val'),
-        etaId: vis1.select('#eta-val'),
-        qSlider: vis1.select('#q-slider'),
-        etaSlider: vis1.select('#eta-slider'),
-        hessType: vis1.select('#hess-type')
-    }
+	const vis1 = d3.select('#vis1')
+	const sels = {
+		quiverPlot: vis1.select('#chart'),
+		qId: vis1.select('#q-val'),
+		etaId: vis1.select('#eta-val'),
+		qSlider: vis1.select('#q-slider'),
+		etaSlider: vis1.select('#eta-slider'),
+		hessType: vis1.select('#hess-type')
+	}
 
-    const op = {
-        qFac: 100,
-        // qFac: 2 // Uncomment if we only care about q=0, 0.5, 1
-        etaFac: 1000
-    }
+	const vizs = {
+		graph: new ContourPlot(sels.quiverPlot)
+	}
 
-    const vizs = {
-        graph: new ContourPlot(sels.quiverPlot)
-    }
+	const defaults = {
+		// Note to also change the default value in the html file!
+		q: 0,
+		eta: 0.1
+	}
 
-    const defaults = {
-        q: 0,
-        eta: 0.01
-    }
+	const scales = {
+		q: d3.scaleLinear().domain([0, 10]).range([0, 1]),
+		eta: d3.scalePow().domain([1, 1000]).range([Math.pow(10, -5), 0.6]).exponent(3)
+	}
 
-    // Initialize graph parameters to match the defaults
-    vizs.graph.q(defaults.q)
-    vizs.graph.eta(defaults.eta)
+	// Initialize graph parameters to match the defaults
+	vizs.graph.q(defaults.q)
+	vizs.graph.eta(defaults.eta)
+	// sels.qSlider.property('value', scales.q.invert(defaults.q))
+	// sels.etaSlider.property('value', scales.q.invert(defaults.eta))
+	sels.qId.text(toQ(defaults.q))
+	sels.etaId.text(toEta(defaults.eta))
 
-    sels.qSlider.attr('value', defaults.q)
-    sels.etaSlider.attr('value', defaults.eta * op.etaFac)
-    sels.qId.text(+sels.qSlider.attr('value') / op.qFac)
-    sels.etaId.text(+sels.etaSlider.attr('value') / op.etaFac)
+	sels.qSlider.on('input', function () {
+		const me = d3.select(this)
+		const v = scales.q(+me.property('value'));
+		vizs.graph.q(v);
+		sels.qId.text(`${toQ(v)}`)
+	})
 
-    sels.qSlider.on('input', function () {
-        const me = d3.select(this)
-        const v = me.property('value') / op.qFac;
-        vizs.graph.q(v);
-        sels.qId.text(`${v}`)
-    })
+	sels.etaSlider.on('input', function () {
+		const me = d3.select(this)
+		const v = scales.eta(me.property('value'));
+		vizs.graph.eta(v)
+		sels.etaId.text(`${toEta(v)}`)
+	})
 
-    sels.etaSlider.on('input', function () {
-        const me = d3.select(this)
-        const v = me.property('value') / op.etaFac;
-        vizs.graph.eta(v)
-        sels.etaId.text(`${v}`)
-    })
-
-    sels.hessType.on('input', function () {
-        const self = d3.select(this)
-        const v = self.property('value')
-        vizs.graph.setUpdater(v)
-    })
+	sels.hessType.on('input', function () {
+		const self = d3.select(this)
+		const v = self.property('value')
+		vizs.graph.setUpdater(v)
+	})
 }
 
 function plotGolfHole() {
-    const vis2 = d3.select("#vis2");
-    const sels = {
-        chart: vis2.select('#chart'),
-        qId: vis2.select('#q-val'),
-        etaId: vis2.select('#eta-val'),
-        qSlider: vis2.select('#q-slider'),
-        etaSlider: vis2.select('#eta-slider'),
-    }
+	const vis2 = d3.select("#vis2");
+	const sels = {
+		chart: vis2.select('#chart'),
+		qId: vis2.select('#q-val'),
+		etaId: vis2.select('#eta-val'),
+		qSlider: vis2.select('#q-slider'),
+		etaSlider: vis2.select('#eta-slider'),
+	}
 
-    const op = {
-        qFac: 100,
-        // qFac: 2 // Uncomment if we only care about q=0, 0.5, 1
-        etaFac: 5000
-    }
+	const vizs = {
+		graph: new GolfHole1D(sels.chart)
+	}
 
-    const vizs = {
-        graph: new GolfHole1D(sels.chart)
-    }
+	const defaults = {
+		// Note to also change the default value in the html file!
+		q: 0,
+		eta: 0.00001
+	}
 
-    const defaults = {
-        q: 0,
-        eta: 0.001
-    }
+	const etaRange = [-5, 2].map(x => Math.pow(10, x))
+	const scales = {
+		q: d3.scaleLinear().domain([0, 10]).range([0, 1]),
+		eta: d3.scalePow().domain([1, 1000]).range(etaRange).exponent(5)
+	}
 
-    // Initialize graph parameters to match the defaults
-    vizs.graph.q(defaults.q)
-    vizs.graph.eta(defaults.eta)
+	// Initialize graph parameters to match the defaults
+	vizs.graph.q(defaults.q)
+	vizs.graph.eta(defaults.eta)
+	sels.qId.text(toQ(defaults.q))
+	sels.etaId.text(toEta(defaults.eta))
 
-    sels.qSlider.attr('value', defaults.q)
-    sels.etaSlider.attr('value', defaults.eta * op.etaFac)
 
-    sels.qId.text(+sels.qSlider.attr('value') / op.qFac)
-    sels.etaId.text(+sels.etaSlider.attr('value') / op.etaFac)
-    vizs.graph.q()
+	sels.qSlider.on('input', function () {
+		const me = d3.select(this)
+		const v = scales.q(me.property('value'));
+		vizs.graph.q(v);
+		console.log(v);
+		sels.qId.text(`${toQ(v)}`)
+	})
 
-    sels.qSlider.on('input', function () {
-        const me = d3.select(this)
-        const v = me.property('value') / op.qFac;
-        vizs.graph.q(v);
-        sels.qId.text(`${v}`)
-    })
-
-    sels.etaSlider.on('input', function () {
-        const me = d3.select(this)
-        const v = me.property('value') / op.etaFac;
-        vizs.graph.eta(v)
-        sels.etaId.text(`${v}`)
-    })
+	sels.etaSlider.on('input', function () {
+		const me = d3.select(this)
+		const v = scales.eta(me.property('value'));
+		vizs.graph.eta(v)
+		sels.etaId.text(`${toEta(v)}`)
+	})
 }
 
 export function main() {
-    console.log("RUNNING");
-    plotQuiverGraph();
-    plotGolfHole();
+	console.log("RUNNING");
+	plotQuiverGraph();
+	plotGolfHole();
 }
 
