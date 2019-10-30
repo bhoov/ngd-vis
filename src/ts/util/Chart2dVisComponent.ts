2d4f530bef0a1c937874e6a87b30154c0246431e
Benjamin Hoover <benjamin.hoover@ibm.com>
2019-10-30 10:31:37 -0400

    Make plots log based

--- src/ts/util/Chart2dVisComponent.ts
+++ src/ts/util/Chart2dVisComponent.ts
@@ -1,10 +1,11 @@
 import * as d3 from 'd3'
+import { Vector2D } from './types'
 import { SVGVisComponent, SVGOptions, MarginInfo } from "./SVGVisComponent"
 
 export interface ChartOptions extends SVGOptions {
     xrange: [number, number]
     yrange: [number, number]
-    pad: MarginInfo // The distance to leave between the main drawing of the graph and any annotations in the margin (each dim should be less than margin dim)
+    pad: MarginInfo             // Distance from edges. Demarcate when things are too close to the edge
 }
 
 export interface ChartScales {
@@ -17,4 +18,20 @@ export interface ChartScales {
 export abstract class Chart2D<DataInterface> extends SVGVisComponent<DataInterface> {
     options: ChartOptions
     scales: ChartScales
+
+    // Put a vector into the coordinate system used by the visualization
+    intoVis(v: Vector2D) {
+        return { x: this.scales.x(v.x), y: this.scales.y(v.y) }
+    }
+
+    // Take a vector in the coordinate system and return it to the math coordinates
+    intoMath(v: Vector2D) {
+        return { x: this.scales.x.invert(v.x), y: this.scales.y.invert(v.y) }
+    }
+
+    // Instantiate the axes of the chart. Reimplement depending on how we want the axes to appear
+    protected abstract createAxes()
+
+    // Instantiate the scales from the xrange and the yrange
+    protected abstract createScales()
 }
--- src/ts/util/types.ts
+++ src/ts/util/types.ts
@@ -1,3 +1,8 @@
+
+export function toVec([x, y]: [number, number]): Vector2D {
+    return { x: x, y: y }
+}
+
 export interface Vector2D {
     x: number
     y: number
