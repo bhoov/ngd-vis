eb434a8935781c485010b1006421823048cbff23
Benjamin Hoover <benjamin.hoover@ibm.com>
2020-01-09 13:27:30 -0500

    Change to dark red

--- src/ts/vis/GolfHole1D.ts
+++ src/ts/vis/GolfHole1D.ts
@@ -219,7 +219,7 @@ export class GolfHole1D extends SVGVisComponent<T> {
 
 
         //@ts-ignore
-        const cscale = d3.scaleLinear().domain([-2, 0, 2]).range(["#67a9cf", "#f7f7f7", "#ef8a62"]).clamp(false)
+        const cscale = d3.scaleLinear().domain([-2, 0, 2]).range(["#67a9cf", "#f7f7f7", "#d90036"]).clamp(false)
 
         sels.lineBackground.selectAll('.grad-box')
             .data(data)
