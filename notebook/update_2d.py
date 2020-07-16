74b0db4b71098cdd5b20b7936cefecf4ed997830
Ben Huh <huh@ibm.com>
2020-07-16 11:58:29 -0700

    deepnet loss / block diagonal approx added

--- notebook/update_2d.py
+++ notebook/update_2d.py
@@ -3,10 +3,10 @@ from fastai.vision import plt
 from scipy.integrate import odeint
 from pdb import set_trace
 
-def get_dyn(loss_fnc, update, rotate = False):
-    def f(w, t, lr, q, damp, normalize = False):
+def get_dyn(loss_fnc, update, rotate = False, Block_diag = False):
+    def f(w, t, lr, q, damp, eps, normalize = False):
         loss, err, jac = loss_fnc(w)
-        dv = update(err, jac, q=q, damping = damp)    #    dv, jac_d, V 
+        dv = update(err, jac, q=q, damping = damp, eps = eps, Block_diag = Block_diag)    #    dv, jac_d, V 
         if normalize:
             dv /= np.sqrt((err ** 2).sum(axis=0))   # normalized update
         if rotate:
@@ -28,43 +28,56 @@ def solver_euler(dyn, w0, t_all, args):
         
     return np.stack(w_all,axis=1).T
 
-def run_exp(loss_fnc, w0, qs = None, lrs = None, dampings = None, OED_solver = None, iter = 100):
+def run_exp(loss_fnc, w0, qs = None, lrs = None, epss = None, dampings = None, OED_solver = None, Block_diag = False, iter = 100):
     w_alls = []; l_alls = []; w_mins = []; w_maxs = []
     
-    if qs is None:         qs = [0.0, 1.0, 0.5]
-    if lrs is None:        lrs = np.array([0.0016, 0.01, 0.03])/4                
-    if dampings is None:   dampings = [0, 10, 0.01]
     if OED_solver is None: 
         OED_solver = solver_euler
     elif OED_solver == 'odeint':
         OED_solver = odeint
-        
+    if qs is None:         qs = [0.0, 1.0, 0.5]
+    if lrs is None:        lrs = np.array([0.0016, 0.01, 0.03])/4                
+    if epss is None:       epss = np.array([100, 0.1, 10])              
+    if dampings is None:   dampings = [0, 10, 0.01]
+       
     t_all = np.linspace(0,iter,iter+1) 
     
-    for q, lr, damp in zip(qs, lrs, dampings):
-        dyn = get_dyn(loss_fnc, qNGD_update )
-        w_all = OED_solver(dyn, w0, t_all, args=(lr, q, damp)).T
+    for q, lr, damp, eps in zip(qs, lrs, dampings, epss):
+        dyn = get_dyn(loss_fnc, qNGD_update, Block_diag = Block_diag )
+        w_all = OED_solver(dyn, w0, t_all, args=(lr, q, damp, eps)).T
         l_all, _, _ = loss_fnc(w_all)
         w_alls.append(w_all);        l_alls.append(l_all);        w_mins.append(w_all.min(axis=1));        w_maxs.append(w_all.max(axis=1))
         
 #         print('V', V, 'err', err, 'V @ err', V @ err, 'j_d', jac_d, 'j_d @  V @ err', jac_d @  V @ err)
     return l_alls, w_alls, (w_mins, w_maxs)
 
-def qNGD_update(err, jac, damping, q=0):
-#     if w.shape[0] == 1:                # 1-d update
-#         update = (jac ) ** (1 - 2*q) * err
-#     elif w.shape[0] == 2:              # 2-d update
 
-    U, d, V = np.linalg.svd(jac)
-    d_ =  d * ((1 + damping)/(d ** 2 + damping)) **q
-    update = U * d_ @ V @ err
-    return update #, d_, V 
+def qNGD_update(err, jac, damping, q, eps, Block_diag = False): 
+    if Block_diag:
+        factor = ((1 + damping)/(jac ** 2 + damping)) #.clip(max = eps) 
+        jac_q = jac * (factor ** q)
+        update = jac_q @ err
+    else:
+        U, d, V = np.linalg.svd(jac)               
+        factor = ((1 + damping)/(d ** 2 + damping)).clip(max = eps) 
+        d_q =  d * (factor ** q)
+        k= d_q.shape[0]
+        update = U[:,:k] * d_q @ V[:k,:] @ err
+    return update 
 
-def qNGD_update_all(err, jac, damping, q=0):
-    U, d, V = svd_2d(jac)   #  np.linalg.svd(jac)
-    d_ =  d * ((1 + damping)/(d ** 2 + damping)) **q
-    update = np.einsum('aik, ik,ijk,jk->ak', U, d_, V, err)
-    return update #, d_, V 
+def qNGD_update_all(err, jac, damping, q, eps, Block_diag = False):
+    if Block_diag:
+        factor = ((1 + damping)/(jac ** 2 + damping)) #.clip(max = eps) 
+        jac_q = jac * (factor ** q)
+        update = np.einsum('ajk,jk->ak', jac_q, err)
+    else:
+        U, d, V = svd_2d(jac)   #  np.linalg.svd(jac)
+        d_ =  d * ((1 + damping)/(d ** 2 + damping)).clip(max = eps)  **q
+        k= d.shape[0]
+    #     update = U[:,:k] * d_q @ V[:k,:] @ err
+    #     set_trace()
+        update = np.einsum('aik, ik,ijk,jk->ak', U[:,:k,:], d_, V[:k,:,:], err)
+    return update 
 
 
 
@@ -93,10 +106,14 @@ def svd_2d(M):   #   SVD of 2x2 matrix M:  U and V are orthogonal, D is diagonal
     return U, d, V
 
 def get_orth_matrix(S):
-    if len(S.shape) == 2:
-        theta   = 0.5*np.arctan2(S[0,1]+S[1,0], S[0,0]-S[1,1])   
-    elif len(S.shape) == 3:
-        theta   = 0.5*np.arctan2(S[0,1,:]+S[1,0,:], S[0,0,:]-S[1,1,:])   
-    Cos, Sin = np.cos(theta), np.sin(theta)
-    return np.array([[Cos, -Sin], [Sin, Cos]])
+    # S: symmetric matrix
+    if S.shape[0] == 1:
+        return np.ones(S.shape)
+    else:    
+        if len(S.shape) == 2:
+            theta   = 0.5*np.arctan2(S[0,1]+S[1,0], S[0,0]-S[1,1])   
+        elif len(S.shape) == 3:
+            theta   = 0.5*np.arctan2(S[0,1,:]+S[1,0,:], S[0,0,:]-S[1,1,:])   
+        Cos, Sin = np.cos(theta), np.sin(theta)
+        return np.array([[Cos, -Sin], [Sin, Cos]])
     
